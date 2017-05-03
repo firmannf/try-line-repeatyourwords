03f35d047efcb3d01decc7dd868106fe88247dfa
firmannf <firmannizammudin@gmail.com>
2017-05-04 03:04:01 +0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use \LINE\LINEBot\SignatureValidator as SignatureValidator;
+
+// load config
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+// initiate app
+$configs =  [
+	'settings' => ['displayErrorDetails' => true],
+];
+$app = new Slim\App($configs);
+
+/* ROUTES */
+$app->get('/', function ($request, $response) {
+	return "Lanjutkan!";
+});
+
+$app->post('/', function ($request, $response)
+{
+	$body 	   = file_get_contents('php://input');
+	$signature = $_SERVER['HTTP_X_LINE_SIGNATURE'];
+
+	file_put_contents('php://stderr', 'Body: '.$body);
+
+	if (empty($signature)){
+		return $response->withStatus(400, 'Signature not set');
+	}
+
+	if($_ENV['PASS_SIGNATURE'] == false && ! SignatureValidator::validateSignature($body, $_ENV['CHANNEL_SECRET'], $signature)){
+		return $response->withStatus(400, 'Invalid signature');
+	}
+
+	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
+	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
+
+	$data = json_decode($body, true);
+	foreach ($data['events'] as $event)
+	{
+		if ($event['type'] == 'message')
+		{
+			if($event['message']['type'] == 'text')
+			{
+				$result = $bot->replyText($event['replyToken'], $event['message']['text']);
+
+				return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+			}
+		}
+	}
+
+});
+
+$app->run();
